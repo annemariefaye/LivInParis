5d0410c9bb57593fe175dd98122d1111212de049
Int <71024388+Intfly@users.noreply.github.com>
2025-05-04 19:09:29 +0200

    Update connexion.cs

--- PbSI/connexion.cs
+++ PbSI/connexion.cs
@@ -48,20 +48,24 @@ namespace PbSI
 
         public void exportGenerauxXML()
         {
-            this.executerRequete("SELECT p.Nom AS NomPlat, p.Prix, u.Nom AS NomCuisinier, u.Prenom AS PrenomCuisinier, p.Nationalite FROM Plat p JOIN Utilisateur u ON p.IdCuisinier = u.Id;");
-            this.exporterResultatRequete("infosPlats");
+            try
+            {
+                this.executerRequete("SELECT p.Nom AS NomPlat, p.Prix, u.Nom AS NomCuisinier, u.Prenom AS PrenomCuisinier, p.Nationalite FROM Plat p JOIN Utilisateur u ON p.IdCuisinier = u.Id;");
+                this.exporterResultatRequete("infosPlats");
 
-            this.executerRequete("SELECT u.idClient, u.Nom, u.Prenom, COUNT(c.IdCommande) AS NombreCommandes FROM Utilisateur u JOIN Commande c ON u.Id = c.IdClient GROUP BY u.Id;");
-            this.exporterResultatRequete("nombreDeCommandesParClient");
+                this.executerRequete("SELECT u.idClient, u.Nom, u.Prenom, COUNT(c.IdCommande) AS NombreCommandes FROM Utilisateur u JOIN Commande c ON u.Id = c.IdClient GROUP BY u.Id;");
+                this.exporterResultatRequete("nombreDeCommandesParClient");
 
-            this.executerRequete("SELECT u.idClient, u.Nom, u.Prenom, SUM(t.Montant) AS TotalDepense FROM Utilisateur u JOIN Commande c ON u.Id = c.IdClient JOIN Transaction t ON c.IdCommande = t.IdCommande WHERE t.Reussie = TRUE GROUP BY u.Id;");
-            this.exporterResultatRequete("totalDepenseParClient");
+                this.executerRequete("SELECT u.idClient, u.Nom, u.Prenom, SUM(t.Montant) AS TotalDepense FROM Utilisateur u JOIN Commande c ON u.Id = c.IdClient JOIN Transaction t ON c.IdCommande = t.IdCommande WHERE t.Reussie = TRUE GROUP BY u.Id;");
+                this.exporterResultatRequete("totalDepenseParClient");
 
-            this.executerRequete("Select * FROM Utilisateur JOIN Client on Client.idClient = Utilisateur.idClient;");
-            this.exporterResultatRequete("infosClients");
-
-            this.executerRequete("Select * FROM Utilisateur JOIN Cuisinier ON Cuisiner.IdCuisinier = Utilisateur.IdCuisinier;");
-            this.exporterResultatRequete("infosCuisiniers");
+                this.executerRequete("Select * FROM Utilisateur JOIN Client on Client.idClient = Utilisateur.idClient;");
+                this.exporterResultatRequete("infosClients");
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         private void CreerBaseSiNonExiste()
